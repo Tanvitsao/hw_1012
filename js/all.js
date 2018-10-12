364439a2d854c872edfb15987dd1930c154211d9
Tanvitsao <kiki5720@gmail.com>
2018-10-13 00:37:47 +0800

    20181012
    iQuery作業

---
+++ js/all.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+	$(".top a").click(function(event) {
+		/* Act on the event */
+		event.preventDefault();
+		$("html,body").animate({scrollTop: 0}, 500);
+	});
+    
+
+
+
+
+
+
+});
